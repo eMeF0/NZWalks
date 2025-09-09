570ecd562de9f2dade366c1395cbb5bdec50ff47
Mateusz Filas <116639452+eMeF0@users.noreply.github.com>
2025-09-09 18:31:23 +0200

    Dodaj pliki projektów.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:7241/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: api/<StudentsController>
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsName = new string[] { "Mateusz", "Jan", "Wiktoria", "Emilia", "Monika"};
+            return Ok(studentsName);ć
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Diffficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Diffficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
